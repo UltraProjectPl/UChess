b7daf4d5a783650587375627d565f22b3f9df530
UltraProjectPl <jakub.sumara@gmail.com>
2020-09-06 20:13:29 +0200

    Draw chessboard

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+const canvas = document.createElement('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+const chessSquareSize = 100;
+const chessPositionX = window.innerWidth / 2 - chessSquareSize * 4;
+const chessPositionY = window.innerHeight / 2 - chessSquareSize * 4;
+
+const drawChessboard = (x: number, y: number, size: number): void => {
+    const startX = x;
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if ((j % 2 === 0 && i % 2 !== 0) || (j % 2 !== 0 && i % 2 === 0)) {
+                ctx.fillStyle = '#67656c';
+            } else {
+                ctx.fillStyle = '#fffdfd';
+            }
+
+            ctx.fillRect(x, y, size, size);
+
+            x += size;
+        }
+
+        x = startX;
+        y += size;
+    }
+};
+
+
+drawChessboard(chessPositionX, chessPositionY, chessSquareSize);
